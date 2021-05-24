94919ab29993813e775dd9a29273c7d0f6124ff0
FishDawg <>
2021-05-23 22:23:21 -0700

    Script improvements

--- slitherioserverlock.js
+++ slitherioserverlock.js
@@ -1,28 +1,56 @@
 // ==UserScript==
-// @name         Slither.io Server Lock
+// @name         Slither Server Lock
 // @namespace    http://fishdawg.com/userscript/slither/serverlock
-// @version      1.0
+// @version      2.0
 // @description  Always stay on the same server in Slither.io. Refresh the page to change servers.
 // @author       FishDawg
-// @match        http://slither.io/
-// @grant        none
+// @match        http*://slither.io/
+// @grant        GM.setValue
+// @grant        GM.getValue
+// @compatible   Greasemonkey
 // @downloadURL  https://github.com/FishDawg/SlitherioServerLock/raw/master/slitherioserverlock.js
 // ==/UserScript==
 
 var lockServerTimerToken;
 
-function lockServer() {
-    if (sos.length > 0) {
-        // Choose a random server just like how the original game does and remove all the other servers from the list
-        sos = [sos[Math.floor(Math.random() * sos.length)]];
+async function lockServer() {
+  if (!bso) {
+    // Only use the saved server from the last 6 hours
+    var cutoffDate = new Date();
+    cutoffDate.setTime(cutoffDate.getTime() - 6*60*60*1000);
 
-        // Remove all clusters from the list (so the game fallback on the list of servers)
-        clus = [];
-
-        window.clearInterval(lockServerTimerToken);
-        lockServerTimerToken = null;
+    // Try to read the saved server
+    var savedDateString = await GM.getValue('Slither Saved Date');
+    if (savedDateString) {
+      var savedDate = new Date(savedDateString);
+      if (!isNaN(savedDate.getTime()) && savedDate >= cutoffDate) {
+        var savedServerString = await GM.getValue('Slither Saved Server');
+        if (savedServerString) {
+          var savedServer = savedServerString;
+          var matchingServer = sos.find(s => s.ip === savedServer);
+          if (matchingServer) {
+            bso = matchingServer;
+            console.log('Loaded saved server: ' + bso.ip);
+          }
+        }
+      }
     }
+  }
+
+  if (bso) {
+    // Keep only the current server
+    sos = [bso];
+
+    // Remove all clusters from the list (so the game fallback on the list of servers)
+    clus = [];
+
+    var currentDate = new Date();
+    GM.setValue('Slither Saved Server', bso.ip);
+    GM.setValue('Slither Saved Date', currentDate.toString());
+
+    console.log('Stored saved server: ' + bso.ip);
+
+    window.clearInterval(lockServerTimerToken);
+    lockServerTimerToken = null;
+  }
 }
-
-lockServerTimerToken = window.setInterval(lockServer, 1000);
-
